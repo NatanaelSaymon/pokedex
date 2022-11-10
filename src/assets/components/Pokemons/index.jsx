c42b50de5239f2597514779c7539a30840e5a1c5
Natanael Saymon <saymoneo@gmail.com>
2022-11-09 23:29:37 -0300

    fix: resolver bug de pokemons duplicados

--- src/assets/components/Pokemons/index.jsx
+++ src/assets/components/Pokemons/index.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import api from '../../../../services/api'
-import axios from 'axios'
 
 import styles from './styles.module.css';
 import pokebola from '../../images/pokebola.png'
@@ -8,36 +7,45 @@ import pokebola from '../../images/pokebola.png'
 export function Pokemons() {
 
     const [pokemon, setPokemon] = useState([])
-
+    
     useEffect(() => {
-        const maxLimits = 151;
-        let urlPokemon = []
-
-        for(let i = 1; i < maxLimits; i++) {
-            urlPokemon.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        const getPokemons = async () => {
+            try {
+                const response = await api.get('/pokemon?limit=150')
+    
+                function getPokemon(results) {
+                    results.forEach(async pokemon => {
+                        const response = await api.get(`/pokemon/${pokemon.name}`)
+                        console.log(response.data)
+                        setPokemon(currentList => [...currentList, response.data])
+                    });
+                }
+    
+                getPokemon(response.data.results)
+    
+            } catch(error) {
+                console.log('ok, temos um erro: ', error)
+            }
         }
 
-        console.log(urlPokemon)
-
-        const response = axios.all(
-            urlPokemon.map((pokemon) => api.get(pokemon).then(data => setPokemon(data)))
-        )
-
-
-        // const loadPokemons = async () => {
-        //     const response = await api.get('/pokemon')
-        //     console.log(response.data.results)
-        // }
-
-        // loadPokemons()
+        getPokemons()
     }, [])
+
 
     return(
         <div className={styles["container"]}>
             <div className={styles["header"]}>
-                <img src={pokebola} alt="Imagen de uma Pokebola" />
+                <img src={pokebola} alt="Imagem de uma Pokebola" />
                 <h1>Pokédex</h1>
             </div>
+
+            {pokemon.length > 0 && (
+                pokemon.map((pokemon) => (
+                    <article key={pokemon.id}>
+                        <p>{pokemon.name}</p>
+                    </article>
+                ))
+            )}
         </div>
     )
 }

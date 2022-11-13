c7a222bd4ee932d1d6a2e1cf3c52ce1b42f9923c
Natanael Saymon <saymoneo@gmail.com>
2022-11-12 22:44:00 -0300

    fix: correção na busca dos dados dos pokemon

--- src/assets/components/Pokemons/index.jsx
+++ src/assets/components/Pokemons/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import api from '../../../../services/api'
+import axios from 'axios';
 
 import styles from './styles.module.css';
 import pokebola from '../../images/pokebola.png'
@@ -9,23 +10,19 @@ export function Pokemons() {
     const [pokemon, setPokemon] = useState([])
     
     useEffect(() => {
-        const getPokemons = async () => {
-            try {
-                const response = await api.get('/pokemon?limit=150')
-    
-                function getPokemon(results) {
-                    results.forEach(async pokemon => {
-                        const response = await api.get(`/pokemon/${pokemon.name}`)
-                        console.log(response.data)
-                        setPokemon(currentList => [...currentList, response.data])
-                    });
-                }
-    
-                getPokemon(response.data.results)
-    
-            } catch(error) {
-                console.log('ok, temos um erro: ', error)
+        const getPokemons = () => {
+            const pokemonUrl = [];
+            const maxLimits = 800;
+
+            for(let i = 1; i <= maxLimits; i++) {
+                pokemonUrl.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
             }
+
+            const response = axios.all(pokemonUrl.map(pokemon => axios.get(pokemon))).then(res => {
+                console.log(res) 
+                return  setPokemon(res)
+            })
+
         }
 
         getPokemons()
@@ -37,12 +34,14 @@ export function Pokemons() {
             <div className={styles["header"]}>
                 <img src={pokebola} alt="Imagem de uma Pokebola" />
                 <h1>Pokédex</h1>
+                
             </div>
 
             {pokemon.length > 0 && (
-                pokemon.map((pokemon) => (
-                    <article key={pokemon.id}>
-                        <p>{pokemon.name}</p>
+                pokemon.map((pokemon, key) => (
+                    <article key={key}>
+                        <p>{pokemon.data.name}</p>
+                        <img src={pokemon.data.sprites.other["official-artwork"].front_default} alt="" />
                     </article>
                 ))
             )}

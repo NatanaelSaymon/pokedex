287efada4d27d57c1849f638a5f3bc499661b2e1
Natanael Saymon <saymoneo@gmail.com>
2022-11-17 22:43:02 -0300

    feat: novas caracteristicas do pokemon

--- src/assets/components/Pokemon/index.jsx
+++ src/assets/components/Pokemon/index.jsx
@@ -1,21 +1,36 @@
 import styles from './styles.module.css'
 
-export function Pokemon({ id, name, img, types }) {
+export function Pokemon({ id, name, img, types, height, weight }) {
     return(
         <div className={styles["pokemon-container"]}>
             <img src={img} alt={name} />
 
-            <div className="description">
-                <p className="descriptionId">{id < 10 ? `#00${id}` : `#${id}`}</p>
-                <h2 className="descriptionName">{name}</h2>
-                <div className="descriptionTypes">
-                    <ul className="descriptionList">
+            <div className={styles["description"]}>
+                <span className={styles["description__id"]}>{id < 10 ? `#00${id}` : `#${id}`}</span>
+                <h2 className={styles["description__name"]}>{name}</h2>
+                <div className={styles["description__types"]}>
+                    <ul className={styles["description__list"]}>
                         {types.map(type => {
                             return(
                                 <li>{type.type.name}</li>
                             )
                         })}
                     </ul>
+                </div>
+
+                <div className={styles["description__caracter"]}>
+                    <div className={styles["description__weight"]}>
+                        <p>
+                            <span><strong>{weight} kg</strong></span>
+                            <span>Peso</span>
+                        </p>
+                    </div>
+                    <div className={styles["description__height"]}>
+                        <p>
+                            <span><strong>{height} m</strong></span>
+                            <span>Altura</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

b74db3874821925b41342fa82431fcf91cd7b247
Natanael Saymon <saymoneo@gmail.com>
2022-12-13 22:44:21 -0300

    feat: exibindo detalhes do pokemon

--- src/assets/components/Pokemon/index.jsx
+++ src/assets/components/Pokemon/index.jsx
@@ -1,31 +1,59 @@
+import { useState } from 'react';
+
+import { Modal } from '../Modal'
 import styles from './styles.module.css'
 
-export function Pokemon({ id, name, img, types }) {
+export function Pokemon({ id, name, img, types, stats, abilities }) {
+
+    const [modalOpen, setModalOpen] = useState(false)
+
+    
     return(
-        <div className={styles["pokemon-container"]}>
-            <div className={styles['pokemon-cover']}>
-                <img src={img} alt={name} />
-            </div>
+        <>
+            <div className={styles["pokemon-container"]} onClick={() => setModalOpen(true)}>
+                <div className={styles['pokemon-cover']}>
+                    <img src={img} alt={name} />
+                </div>
 
-            <div className={styles["description"]}> 
-                <span className={styles["description__id"]}>
-                    {id < 10 ? `#00${id}` : `#${id}`}
-                </span>
+                <div className={styles["description"]}> 
+                    <span className={styles["description__id"]}>
+                        {id < 10 ? `#00${id}` : `#${id}`}
+                    </span>
 
-                <h2 className={styles["description__name"]}>
-                    {name}
-                </h2>
+                    <h2 className={styles["description__name"]}>
+                        {name}
+                    </h2>
 
-                <div className={styles["description__types"]}>
-                    <ul className={styles["description__list"]}>
-                        {types.map(type => {
-                            return(
-                                <li>{type.type.name}</li>
-                            )
-                        })}
-                    </ul>
+                    <div className={styles["description__types"]}>
+                        <ul className={styles["description__list"]}>
+                            {types.map(type => {
+                                return(
+                                    <li>{type.type.name}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <Modal isOpen={modalOpen} setIsOpen={setModalOpen}>
+                {name}
+                <ul>
+                    {stats.map(stat => {
+                        return(
+                            <li>{stat.stat.name} - {stat.base_stat}</li>
+                        )
+                    })}
+                </ul>
+
+                <ul>
+                    {abilities.map(ability => {
+                        return(
+                            <li>{ability.ability.name}</li>
+                        )
+                    })}
+                </ul>
+            </Modal>
+        </>
     )
 }
